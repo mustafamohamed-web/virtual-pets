570a9d9970254b43ece8a9497d543e07dc49dc0d
mustafa mohammed <mustafamohammed@mustafas-MacBook-Pro.local>
2021-07-11 21:17:55 +0100

    age function

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -4,7 +4,16 @@ describe('constructor', () => {
   
     it('sets the name property', () => {
       const pet = new Pet('Fido');
-  
       expect(pet.name).toEqual('Fido');
+    });
+  });
+
+  describe('growUp', () => {
+    it('increments the age by 1', () => {
+      const pet = new Pet('Fido');
+  
+      pet.growUp();
+  
+      expect(pet.age).toEqual(1);
     });
   });
--- src/pet.js
+++ src/pet.js
@@ -1,9 +1,13 @@
 function Pet(name) {
  this.name = name;
+this.age = 0;
+this.growUp = function(){
+    this.age+=1
+}
 }
 
 const pet = new Pet('Fido');
 
-pet.name;
+
 
 module.exports = Pet;
