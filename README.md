66da494bc850b114ceb122c4d20faedc7d5900d2
mustafa mohammed <mustafamohammed@mustafas-MacBook-Pro.local>
2021-07-12 09:25:26 +0100

    AGE FUNCTION

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 This is the read me for the virtual pets project using oop
 Added a name feature to the object and updated this using testing
 Added a age feature where it starts at 0 and increments by 1 
+Added another person to the object and incremented age by 10 everytime function is called.
--- src/pet.js
+++ src/pet.js
@@ -1,12 +1,25 @@
-function Pet(name) {
- this.name = name;
-this.age = 0;
-this.growUp = function(){
-    this.age+=1
-}
-}
-
-const pet = new Pet('Fido');
+function Pet(name, age){
+    this.name = name;
+    this.age = age;
+    };
+  
+  Pet.prototype = {
+    growUp : function() {this.age += 1;}
+  }
+  
+  const pet = new Pet('Fido', 10)
+  const rex = new Pet('Rex', 5)
+  
+  rex.growUp = function(){
+    this.age += 5;
+  }
+  
+  rex.growUp()
+  rex.growUp()
+  
+  pet.growUp = function(){
+    this.age+=10
+  }
 
 
 
