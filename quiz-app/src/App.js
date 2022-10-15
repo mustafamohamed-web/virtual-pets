bfb0e3b44259955ac46f414edb7bab3f9c7a3a83
mustafa mohammed <mustafamohammed@mustafas-MacBook-Pro.local>
2022-10-15 15:58:42 +0100

    context + api setup

---
+++ quiz-app/src/App.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import { useGlobalContext } from "./context";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
---
+++ quiz-app/src/components/SetupForm.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const setupForm = () => {
+  return <div>setupForm</div>;
+};
+
+export default setupForm;
